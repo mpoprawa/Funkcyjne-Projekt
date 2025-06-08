72278c4f033dda61babe77a413bcb2f445323d1c
mp <mp@Ubuntu.myguest.virtualbox.org>
2025-06-08 13:28:59 +0200

    components

--- graph.hs
+++ graph.hs
@@ -2,6 +2,7 @@ import System.IO
 import Data.Char
 import Data.List
 import Data.Maybe
+import qualified Data.IntSet as IntSet
 
 type Node = Int
 type Graph = [(Node, [Node])]
@@ -36,7 +37,33 @@ loadGraph path = do
         parsed = mapMaybe parseLine fileLines
     return parsed
 
+getNeighbors :: Graph -> Node -> [Node]
+getNeighbors graph node = fromMaybe [] (lookup node graph)
+
+connectedComponents :: Graph -> [[Node]]
+connectedComponents graph = findComponents allNodes IntSet.empty []
+    where
+        allNodes = map fst graph
+        findComponents  [] _ components = components
+        findComponents  (n:rest) visited components
+            | IntSet.member n visited = findComponents  rest visited components
+            | otherwise =
+                let (component, visited') = dfs graph n visited
+                    in findComponents rest visited' (component : components)
+
+dfs :: Graph -> Node -> IntSet.IntSet -> ([Node], IntSet.IntSet)
+dfs graph start visited = runDfs [start] visited []
+    where
+        runDfs [] vis acc = (reverse acc, vis)
+        runDfs (n:rest) vis acc
+            | IntSet.member n vis = runDfs rest vis acc
+            | otherwise =
+                let neighbors = getNeighbors graph n
+                    vis' = IntSet.insert n vis
+                in runDfs (neighbors ++ rest) vis' (n : acc)
+
 main :: IO ()
 main = do
-    graph <- loadGraph "example.txt"
+    graph <- loadGraph "components.txt"
     print graph
+    print (connectedComponents graph)
